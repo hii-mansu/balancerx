d8a9967534d0401eef193e3ad289c51c1f37c8fd
Himanshu Singh <himanshusingh5067@gmail.com>
2026-07-21 16:15:11 +0530

    Feat: Implimented Request & Error Logging

--- src/proxy/proxy.js
+++ src/proxy/proxy.js
@@ -3,9 +3,11 @@ import {
   incrementConnections,
   decrementConnections,
 } from "../connections/connectionStore.js";
+import { logError, logRequest } from "../logger/logger.js";
 
 export function proxyRequest(req, res, target) {
   return new Promise((resolve, reject) => {
+    const startTime = Date.now();
     const url = new URL(target);
 
     const options = {
@@ -25,12 +27,29 @@ export function proxyRequest(req, res, target) {
 
       proxyRes.on("end", () => {
         decrementConnections(target);
+
+        logRequest({
+          method: req.method,
+          url: req.url,
+          target,
+          statusCode: proxyRes.statusCode,
+          duration: Date.now() - startTime,
+        });
+
         resolve();
       });
     });
 
     proxyReq.on("error", (error) => {
       decrementConnections(target);
+
+      logError({
+        method: req.method,
+        url: req.url,
+        target,
+        error,
+      });
+
       reject(error);
     });
 
